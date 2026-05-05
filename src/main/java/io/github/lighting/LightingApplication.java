45d622a614da58b3da1547aa18acbdec9f64e947
Jaison Staloch Junior | Desktop-DevCourse <staloch.dev@gmail.com>
2026-05-04 21:00:40 -0300

    feat: initial commit

---
+++ src/main/java/io/github/lighting/LightingApplication.java
@@ -0,0 +1,13 @@
+package io.github.lighting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/lighting/LightingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lighting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
